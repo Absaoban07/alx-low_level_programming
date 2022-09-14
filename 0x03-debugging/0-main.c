223d01e6f4bded75ae657dc8fd796b7947b89b85
Absaoban07 <absaoban@gmail.com>
2022-09-14 01:26:40 -0700

    Task 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,28 @@
 #include "main.h"
+#include <stdlib.h>
+#include <time.h>
+
+void positive_or_negative(int i)
+{
+int n;
+
+srand(time(0));
+n = i
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
+
 /**
  * main - tests function that prints if integer	is positive or negative
  *
@@ -8,7 +32,7 @@ int main(void)
 {
 int i;
 
-i = 98;
+i = 0;
 positive_or_negative(i);
 return (0);
 }
