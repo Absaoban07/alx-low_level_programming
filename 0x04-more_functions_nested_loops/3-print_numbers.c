0b99459086f851b1d3f815e1229d76c408ee81b6
Absaoban07 <absaoban@gmail.com>
2022-09-15 07:19:56 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_numbers - print numbers to the terminal and return void
+*
+* Description: print numbers from 0 to 9 to the terminal
+* Return: void
+*/
+
+void print_numbers(void)
+{
+	int n;
+
+	n = '0';
+		while (n < 10)
+		{
+			putchar('0' + n);
+			n++;
+		}
+	putchar('\n');
+}
+
