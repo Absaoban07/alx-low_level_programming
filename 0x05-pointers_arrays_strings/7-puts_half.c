c2cc40186d3087990869fab882a83bcf649873ae
Absaoban07 <absaoban@gmail.com>
2022-09-19 07:50:21 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,17 +10,17 @@
 
 void puts_half(char *str)
 {
-	int len = 0;
-	while (*str != '\0')
-	{
-		len++;
-		str++;
-	}
-	str -= (len / 2);
-	while (*str != '\0')
-	{
-		putchar(*str);
-		str++;
-	}
-	putchar('\n');
+int len = 0;
+while (*str != '\0')
+{
+len++;
+str++;
+}
+str -= (len / 2);
+while (*str != '\0')
+{
+putchar(*str);
+str++;
+}
+putchar('\n');
 }
