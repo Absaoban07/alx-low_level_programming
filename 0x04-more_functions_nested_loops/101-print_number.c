075d127042d6bbc5c5332d96f45226dc7179ca9a
Absaoban07 <absaoban@gmail.com>
2022-09-20 19:45:29 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,25 +8,14 @@
 
 void print_number(int n)
 {
-unsigned int i, j, count;
+unsigned int x;
+x = n;
 if (n < 0)
 {
 putchar(45);
-i = n * -1;
+x = -n;
 }
-else
-{
-i = n;
-}
-j = i;
-count = 1;
-while (j > 9)
-{
-j /= 10;
-count *= 10;
-}
-for (; count >= 1; count /= 10)
-{
-putchar(((i / count) % 10) + 48);
-}
+if (x / 10)
+print_number(x / 10);
+putchar((x % 10) + '0');
 }
