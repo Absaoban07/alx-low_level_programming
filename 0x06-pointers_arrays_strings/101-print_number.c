1ed4577c1bee857079a196a44b2cb8433f6f39b3
Absaoban07 <absaoban@gmail.com>
2022-09-21 20:47:36 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,12 +14,12 @@ unsigned int num;
 num = n;
 if (n < 0)
 {
-putchar(45);
+_putchar(45);
 num = -n;
 }
 if (num / 10)
 {
 print_number(num / 10);
 }
-putchar((num % 10) + '0');
+_putchar((num % 10) + '0');
 }
